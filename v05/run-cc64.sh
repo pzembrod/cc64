692865305c853999b327007466acb4c836e72bd4
Philip Zembrod <pzembrod@gmail.com>
2020-03-15 23:48:16 +0100

    Switch running cc64 in vice to use all P00 files

---
+++ v05/run-cc64.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Run Vice emulator with charset adapted for use with C as generated
+# by c-char-rom-gen, namely including the chars \^_{|}~ needed by C.
+
+set -e
+
+x64 \
+  -virtualdev \
+  +truedrive \
+  -drive8type 1541 \
+  -fs8 . \
+  -fs8savep00 \
+  -fs8hidecbm \
+  -chargen c-chargen \
