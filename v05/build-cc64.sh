b5f0892207453cb1d9b7e98d2a89cf28ac6ea926
Philip Zembrod <pzembrod@gmail.com>
2020-03-22 22:51:07 +0100

    Add peddi source to cc64 build script

--- v05/build-cc64.sh
+++ v05/build-cc64.sh
@@ -7,8 +7,10 @@ x64 \
   -drive8type 1541 \
   -drive9type 1541 \
   -drive10type 1541 \
+  -drive11type 1541 \
   -fs8 . \
   -fs8savep00 \
   -fs8hidecbm \
   -9 ./src/cc64src1.d64 \
-  -10 ./src/cc64src2.d64
+  -10 ./src/cc64src2.d64 \
+  -11 ./src/peddi_src.d64
