13a18b4cecb2f7bc8850811b844feb4003881fa8
Philip Zembrod <pzembrod@gmail.com>
2020-04-27 08:49:17 +0200

    First part of an automated smoke test in Vice, yet without verification of test program output.

---
+++ v05/tests/compile-in-vice.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+set -e
+
+basedir="$(dirname "${BASH_SOURCE[0]}")"
+
+keybuf=""
+if [ -n "$1" ]
+then
+  keybuf="$1"
+  ascii2petscii "${basedir}/../notdone" "${basedir}/c64files/notdone"
+fi
+
+x64 \
+  -virtualdev \
+  +truedrive \
+  -drive8type 1541 \
+  -fs8 "${basedir}/c64files" \
+  -chargen "${basedir}/../c-chargen" \
+  -symkeymap "${basedir}/../x11_sym_uf_de.vkm" \
+  -autostart "${basedir}/../cc64v05.T64" \
+  -keybuf "$keybuf" \
+  -warp \
+  &
+
+
+if [ -n "$keybuf" ]
+then
+  while [ -f "${basedir}/c64files/notdone" ]
+    do sleep 1
+  done
+  sleep 0.5
+
+  kill %1
+fi
+
+wait %1 || echo "x64 returned $?"
---
+++ v05/tests/compile.fth
@@ -0,0 +1,7 @@
+
+
+also shell
+
+cc helloworld.c
+
+dos s0:notdone
