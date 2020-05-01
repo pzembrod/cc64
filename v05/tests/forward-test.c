468ab5e7f95555487c96b1f77a0c4dd6e4ee8eab
Philip Zembrod <pzembrod@gmail.com>
2020-05-01 19:27:00 +0200

    Update forward-test.c with commended-out code that exposes adjust-prototype breakage.

--- v05/tests/forward-test.c
+++ v05/tests/forward-test.c
@@ -1,5 +1,14 @@
 
+extern function();
+
 forward()
+{
+  puts("forward\n"); /* TODO: replace with: function(); */
+}
+
+/* TODO: enable once adjust-prototype has been fixed (issue 5). 
+function()
 {
   puts("forward\n");
 }
+/* */
