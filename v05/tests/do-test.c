1b336e854164336adb7a283c35fed94aa267fdac
Philip Zembrod <pzembrod@gmail.com>
2020-05-08 23:24:28 +0200

    add do-loop test

---
+++ v05/tests/do-test.c
@@ -0,0 +1,33 @@
+
+do_test() {
+  int i, j;
+  println("Before false-condition do loop");
+  do {
+    println("This should be printed once.");
+  } while(0);
+  println("After false-condition do loop");
+  
+  i = 4;
+  println("Before 4-iteration do loop");
+  do {
+    print("Iteration "); println(itoa(i));
+    --i;
+  } while(i);
+  println("After 4-iteration do loop");
+
+  i = 3;
+  println("Before 3-2-iteration nested do loops");
+  do {
+    print("Outer iteration "); println(itoa(i));
+    j = 2;
+    do {
+      print("Inner iteration "); print(itoa(i));
+      print(" , "); println(itoa(j));
+      j--;
+    } while(j);
+    --i;
+  } while(i);
+  println("After 3-2-iteration nested do loops");
+
+  evaluateAsserts();
+}
