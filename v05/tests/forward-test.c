dc52eb791ddc6308f0cc8cfda18dffe7ecf40bdf
Philip Zembrod <pzembrod@gmail.com>
2020-05-22 12:48:35 +0200

    Make forward-test.c a bit more specific - distinguish first and 2nd call

--- v05/tests/forward-test.c
+++ v05/tests/forward-test.c
@@ -4,22 +4,24 @@ extern forward_declared();
 int prototype_address;
 int final_address;
 
-middle_function()
+middle_function(i)
+int i;
 {
   prototype_address = forward_declared;
-  forward_declared();
+  forward_declared(i);
 }
 
-forward_declared()
+forward_declared(i)
+int i;
 {
-  println("forward");
+  print("forward "); println(itoa(i));
 }
 
 forward_test()
 {
   final_address = forward_declared;
-  middle_function();
-  forward_declared();
+  middle_function(1);
+  forward_declared(2);
 
   assertTrue(prototype_address - middle_function < 0,
       "prototype function address < muddle function address");
