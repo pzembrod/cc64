a9d35e3b5c0842384ea7447aa19d5070d67757b1
Philip Zembrod <pzembrod@gmail.com>
2020-06-05 20:56:58 +0200

    Copy v05/tests/peddi to tests/peddi, with adapted Makefile and scripts

---
+++ tests/peddi/run-peddi.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+
+testdir="$(realpath --relative-to="$PWD" "$(dirname "${BASH_SOURCE[0]}")")"
+basedir="$(realpath --relative-to="$PWD" "${testdir}/../..")"
+emulatordir="$(realpath --relative-to="$PWD" "${basedir}/emulator")"
+c64files="$(realpath --relative-to="$PWD" "${testdir}/c64files")"
+
+peddi="peddi"
+keybuf=""
+warp=""
+if [ -n "$2" ]
+then
+  # keybuf="$1"
+  keybuf="${2}dos s0:notdone\n"
+  warp="-warp"
+  ascii2petscii "${testdir}/notdone" "${c64files}/notdone"
+fi
+
+if [ -n "$1" ]
+then
+  peddi="$1"
+fi
+
+x64 \
+  -virtualdev \
+  +truedrive \
+  -drive8type 1541 \
+  -fs8 "${c64files}" \
+  -chargen "${emulatordir}/c-chargen" \
+  -symkeymap "${emulatordir}/x11_sym_uf_de.vkm" \
+  -keymap 2 \
+  -autostart "${emulatordir}/${peddi}.T64" \
+  -keybuf "$keybuf" \
+  $warp \
+  &
+
+
+if [ -n "$keybuf" ]
+then
+  while [ -f "${c64files}/notdone" ]
+    do sleep 1
+  done
+  sleep 0.5
+
+  kill %1
+fi
+
+wait %1 || echo "x64 returned $?"
---
+++ tests/peddi/run-test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run peddi with script
+peddi="$1"
+testname="$2"
+rm -f "c64files/${testname}.txt" "${testname}.txt"
+if [ -f "${testname}.before" ]
+then
+  ascii2petscii "${testname}.before" "c64files/${testname}.txt"
+fi
+./run-peddi.sh "$peddi" "$(printf "ed ${testname}.txt\n\
+$(sed -e 's/\\/\\\\/g' ${testname}.keybuf | tr -d '\n')")"
+petscii2ascii "c64files/${testname}.txt" "${testname}.out"
+
+# Evaluate test output
+echo "Test: $testname" > tmp.result
+diff "${testname}.golden" "${testname}.out" >> tmp.result
+result=$?
+test $result -eq 0 \
+  && echo "PASS: $testname" >> tmp.result \
+  || echo "FAIL: $testname" >> tmp.result
+cat tmp.result
+mv tmp.result "${testname}-result.txt"
+exit $result
