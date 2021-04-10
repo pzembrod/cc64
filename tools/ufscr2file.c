bdfb48ac81cbd4e1bbe6bf2248056b7ed635cdce
Philip Zembrod <pzembrod@gmail.com>
2021-04-10 15:21:05 +0200

    Add conversion of C64 VolksForth screen disks with shadows to ufscr2file.c

--- tools/ufscr2file.c
+++ tools/ufscr2file.c
@@ -7,6 +7,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "openfiles.h"
 #include "petscii.h"
@@ -37,14 +38,19 @@ int nextchar(FILE *in) {
 }
 
 void buffered_putchar(FILE *out, char c) {
-  static char line[50], *p=line;
-  if (p - line > 45) {
+  static char line[100], *p=line;
+  if (p - line > 95) {
     fprintf(stderr, "!!! overlong line !!!\n");
     exit(1);
   }
   if (c == '\n') {
     while (p > line && *(p - 1) == ' ') p--;
-    *p = '\0';
+    *p-- = '\0';
+    if (p > line + 40 && *p == '\\' && *(p-1) == ' ') {
+      p -= 2;
+      while (p > line && *(p - 1) == ' ') p--;
+      *p-- = '\0';
+    }
     fprintf(out, "%s\n", line);
     p=line;
     return;
@@ -52,7 +58,7 @@ void buffered_putchar(FILE *out, char c) {
   *p++ = c;
 }
 
-int printblock(FILE *in, FILE *out, int n) {
+int readwriteblock(FILE *in, FILE *out, int n) {
   int line, col, c;
   fprintf(out, "\n\\ *** Block No. %d, Hexblock %x\n\n", n, n);
   for (line = 0; line < 25; line++) {
@@ -69,17 +75,122 @@ int printblock(FILE *in, FILE *out, int n) {
   return 0;
 }
 
+#define BLOCKSIZE 1024
+
+int readblock(FILE *in, short buf[], int n) {
+  int i, c;
+  for (i = 0; i < BLOCKSIZE; ++i) {
+    c = nextchar(in);
+    if (c == EOF) {
+      fprintf(stderr, "\n### premature end of input file ###\n");
+      return 1;
+    }
+    buf[n * BLOCKSIZE + i] = c;
+  }
+  return 0;
+}
+
+int writeblock(FILE *out, short buf[], int n) {
+  int line, col, i;
+  fprintf(out, "\n\\ *** Block No. %d, Hexblock %x\n\n", n, n);
+  for (line = 0, i = 0; line < 25; ++line) {
+    for (col = 0; col < (line == 24 ? 40 : 41); col++, ++i) {
+      buffered_putchar(out, (char) petscii2ascii(buf[n * BLOCKSIZE + i]));
+    }
+    buffered_putchar(out, '\n');
+  }
+  return 0;
+}
+
+int writeshadowedblock(FILE *out, short buf[], int block, int shadow) {
+  int line, col, i;
+  short *bp, *sp;
+  fprintf(out, "\n\\ *** Block No. %d, Hexblock %x\n\n", block, block);
+  bp = buf +  block * BLOCKSIZE;
+  sp = buf + shadow * BLOCKSIZE;
+  for (line = 0, i = 0; line < 25; ++line) {
+    for (col = 0; col < (line == 24 ? 40 : 41); col++, ++bp) {
+      buffered_putchar(out, (char) petscii2ascii(*bp));
+    }
+    if (24 == line) {
+      buffered_putchar(out, ' ');
+    }
+    buffered_putchar(out, ' ');
+    buffered_putchar(out, '\\');
+    buffered_putchar(out, ' ');
+    for (col = 0; col < (line == 24 ? 40 : 41); col++, ++sp) {
+      buffered_putchar(out, (char) petscii2ascii(*sp));
+    }
+    buffered_putchar(out, '\n');
+  }
+  return 0;
+}
+
+#define MAX_BLOCK 170
+#define HALF_MAX_BLOCK (MAX_BLOCK / 2)
+
+int readwritedisk(FILE *in, FILE *out) {
+  for (int block = 0; block < MAX_BLOCK; block++)
+    if (readwriteblock(in, out, block))
+      return EXIT_FAILURE;
+  return 0;
+}
+
+int readwritediskbuffered(FILE *in, FILE *out) {
+  short buffer[MAX_BLOCK * BLOCKSIZE];
+  int block;
+  for (block = 0; block < MAX_BLOCK; block++)
+    if (readblock(in, buffer, block))
+      return EXIT_FAILURE;
+  for (block = 0; block < MAX_BLOCK; block++)
+    if (writeblock(out, buffer, block))
+      return EXIT_FAILURE;
+  return 0;
+}
+
+int readwritediskshadowed(FILE *in, FILE *out) {
+  short buffer[MAX_BLOCK * BLOCKSIZE];
+  int block;
+  for (block = 0; block < MAX_BLOCK; block++)
+    if (readblock(in, buffer, block))
+      return EXIT_FAILURE;
+  for (block = 0; block < HALF_MAX_BLOCK; block++)
+    if (writeshadowedblock(out, buffer, block, block + HALF_MAX_BLOCK))
+      return EXIT_FAILURE;
+  return 0;
+}
+
+enum mode { unbufferd, buffered, shadowed };
+
 int main(int argc, char *argv[]) {
   FILE *in, *out;
+  enum mode m = unbufferd;
+  if (argc >= 2 && strcmp(argv[1], "-b") == 0) {
+    m = buffered;
+    --argc; ++argv;
+  } else if (argc >= 2 && strcmp(argv[1], "-s") == 0) {
+    m = shadowed;
+    --argc; ++argv;
+  }
   int error = openfiles(&in, &out, argc, argv,
       "usage: %s file.d64 [outfile]\n");
   if (error) {
     return error;
   }
-  int block;
-  for (block = 0; block < 170; block++)
-    if (printblock(in, out, block))
-      return EXIT_FAILURE;
+  switch(m) {
+    case unbufferd:
+      if (readwritedisk(in, out))
+        return EXIT_FAILURE;
+      break;
+    case buffered:
+      if (readwritediskbuffered(in, out))
+        return EXIT_FAILURE;
+      break;
+    case shadowed:
+      if (readwritediskshadowed(in, out))
+        return EXIT_FAILURE;
+      break;
+    }
   return closefiles(in, out);
 }
 
