c8a8b84c626b5b094365b7486c1f036d5d820ad9
Philip Zembrod <pzembrod@gmail.com>
2021-06-21 09:34:15 +0200

    Test oracle for rand() function.

---
+++ tests/lib/rand-oracle.c
@@ -0,0 +1,27 @@
+
+#include <stdlib.h>
+#include <stdio.h>
+
+unsigned int next = 1;
+
+int myrand( void ) {
+  next = next * 1103515245 + 12345;
+  next &= 0xffffffff;
+  return (int) (next >> 16) & 0x7fff;
+}
+
+void mysrand(int seed) {
+  next = seed;
+}
+
+int main(int argc, char *argv[]) {
+  int count = argc >= 2 ? atoi(argv[1]) : 5;
+  if (argc >= 3) { mysrand(atoi(argv[2])); }
+  char *expected = argc >= 4 ? argv[3] : "expected";
+  printf("int %s[] = {", expected);
+  for (int i = 0; i < count - 1; ++i) {
+    printf("%d, ", myrand());
+  }
+  printf("%d};\n", myrand());
+  return 0;
+}
