f2392ebf8a6e4e2533c10c52cc7f8240f8b2d0ba
Philip Zembrod <pzembrod@gmail.com>
2021-10-03 23:54:46 +0200

    Rename allocfh/freefh() to _allocfh/_freefh()

--- tests/lib/stdio/allocfh-test.c
+++ tests/lib/stdio/allocfh-test.c
@@ -1,30 +1,30 @@
 
 allocfh_test() {
-  assertEq(allocfh(), 7);
-  assertEq(allocfh(), 8);
-  assertEq(allocfh(), 9);
-  assertEq(allocfh(), 10);
-  assertEq(allocfh(), 11);
-  assertEq(allocfh(), 12);
-  assertEq(allocfh(), 13);
-  assertEq(allocfh(), 14);
-  assertEq(allocfh(), 0);
-  assertEq(allocfh(), 0);
-  assertEq(freefh(11), 11);
-  assertEq(allocfh(), 11);
-  assertEq(allocfh(), 0);
-  assertEq(freefh(7), 7);
-  assertEq(allocfh(), 7);
-  assertEq(allocfh(), 0);
-  assertEq(allocfh(), 0);
-  assertEq(freefh(7), 7);
-  assertEq(freefh(8), 8);
-  assertEq(freefh(9), 9);
-  assertEq(freefh(10), 10);
-  assertEq(freefh(11), 11);
-  assertEq(freefh(12), 12);
-  assertEq(freefh(13), 13);
-  assertEq(freefh(14), 14);
+  assertEq(_allocfh(), 7);
+  assertEq(_allocfh(), 8);
+  assertEq(_allocfh(), 9);
+  assertEq(_allocfh(), 10);
+  assertEq(_allocfh(), 11);
+  assertEq(_allocfh(), 12);
+  assertEq(_allocfh(), 13);
+  assertEq(_allocfh(), 14);
+  assertEq(_allocfh(), 0);
+  assertEq(_allocfh(), 0);
+  assertEq(_freefh(11), 11);
+  assertEq(_allocfh(), 11);
+  assertEq(_allocfh(), 0);
+  assertEq(_freefh(7), 7);
+  assertEq(_allocfh(), 7);
+  assertEq(_allocfh(), 0);
+  assertEq(_allocfh(), 0);
+  assertEq(_freefh(7), 7);
+  assertEq(_freefh(8), 8);
+  assertEq(_freefh(9), 9);
+  assertEq(_freefh(10), 10);
+  assertEq(_freefh(11), 11);
+  assertEq(_freefh(12), 12);
+  assertEq(_freefh(13), 13);
+  assertEq(_freefh(14), 14);
 
   evaluateAsserts();
 }
