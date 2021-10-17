c0fab288b56e15f9f22ce186d3f7ae0dff1531eb
Philip Zembrod <pzembrod@gmail.com>
2021-10-18 00:03:36 +0200

    First working version of fopen(), fclose(), fgetc(), fgets(). fopen() only tested for reading a file yet.

--- tests/lib/stdio/fopen-test.c
+++ tests/lib/stdio/fopen-test.c
@@ -7,15 +7,46 @@ fopen_test() {
      my system is at once less forgiving (segfaults on mode NULL) and more
      forgiving (accepts undefined modes).
   */
-  int fh;
+  int fh, c;
+  static char buffer[100], *p;
 
   remove(testfile);
   assertEq(fopen(0, 0), 0, "fopen(0, 0)");
   assertEq(fopen(0, "w"), 0, "fopen(0, \"w\")");
   assertEq(fopen("", 0), 0, "fopen(\"\", 0)");
   assertEq(fopen("", "w"), 0, "fopen(\"\", \"w\")");
-  assertEq(fopen(testfile, 0), 0, "fopen(testfile, 0)");
-  assertEq(fopen(testfile, ""), 0, "fopen(testfile, \"\")");
+
+  assertTrue((fh = fopen("libc-c64.h,s,r", 0)) != 0,
+      "fopen(libc.h, 0)");
+  assertEq(fh, 7, "fh");
+  assertEq(tst_kernal_fnam_len, 14, "tst_kernal_fnam_len");
+  c = fgetc(fh);
+  assertEq(c, 13, "fgetc(fh)");
+  p = fgets(buffer, 8, fh);
+  assertEq(p, buffer, "p = buffer");
+  assertEq(strlen(buffer), 8, "strlen(buffer)");
+  assertEq(strcmp(buffer, "#pragma "), 0,
+      "strcmp(buffer, \"#pragma \")");
+  assertEq(fclose(fh), 0, "flcose(fh)");
+  c = fgetc(fh);
+  assertEq(c, -1, "fgetc(fh) EOF");
+  assertEq(errno, 3, "errno");
+
+  evaluateAsserts();
+  return;
+
+  assertTrue((fh = fopen(testfile, "2")) != 0, "fopen(testfile, \"\")");
+  assertEq(fh, 8, "fh");
+  assertEq(tst_kernal_fnam_len, 9, "tst_kernal_fnam_len");
+  assertEq(strncmp(tst_kernal_fnam, "testfile2", 9), 0,
+      "strncmp(tst_kernal_fnam, testfile1)");
+
+  assertTrue((fh = fopen(testfile, ",s,w")) != 0,
+      "fopen(testfile, \",s,w\")");
+  assertEq(fh, 9, "fh");
+  assertEq(tst_kernal_fnam_len, 12, "tst_kernal_fnam_len");
+  assertEq(strncmp(tst_kernal_fnam, "testfile,s,w", 12), 0,
+      "strncmp(tst_kernal_fnam, testfile,s,w)");
 
   /* assertTrue(0, "okay, this triggered"); /**/
   evaluateAsserts();
