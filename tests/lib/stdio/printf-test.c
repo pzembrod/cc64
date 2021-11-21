006cca3a0532f60cbf2b4385682e15d52e3e3527
Philip Zembrod <pzembrod@gmail.com>
2021-11-21 22:26:26 +0100

    Add %x implementation to printf()

--- tests/lib/stdio/printf-test.c
+++ tests/lib/stdio/printf-test.c
@@ -5,6 +5,9 @@ printf_test() {
   printf("foo%%bar\n", 0x41);
   printf("c:%c:\n", 0x40);
   printf("foo-%sbaz\n", "bar-");
+  printf("%s = $%x\n", "cold start", 64738);
+  printf("%s %s = $%x\n", "tape", "buffer", 828);
+  printf("asc('(') = $%x asc(CR) = $%x\n", '(', '\n');
   tst_clrchn();
 
   evaluateAsserts();
