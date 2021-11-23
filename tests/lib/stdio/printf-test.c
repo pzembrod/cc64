a4b4da7965d8f48e10bb0052fa80ed24dff059e5
Philip Zembrod <pzembrod@gmail.com>
2021-11-23 12:53:04 +0100

    Remove leading zeros from printf("%d", 10)

--- tests/lib/stdio/printf-test.c
+++ tests/lib/stdio/printf-test.c
@@ -8,7 +8,8 @@ printf_test() {
   printf("%s = $%x\n", "cold start", 64738);
   printf("%s %s = $%x\n", "tape", "buffer", 828);
   printf("asc('(') = $%x asc(CR) = $%x\n", '(', '\n');
-  printf("12345 = %d ; ten = %d\n", 12345, 10);
+  printf("12345 = %d ; ten = %d ; two = %d ; zero = %d\n",
+      12345, 10, 2, 0);
   tst_clrchn();
 
   evaluateAsserts();
