fbad88b3d2c0d757050f3721818bf0f3f7d032f8
Philip Zembrod <pzembrod@gmail.com>
2021-11-24 13:52:23 +0100

    Implement printf("%u"), add sign handling to "%d". Also add first test for printf return value.

--- tests/lib/stdio/printf-test.c
+++ tests/lib/stdio/printf-test.c
@@ -1,7 +1,7 @@
 
 printf_test() {
   tst_chkout(tst_file_no);
-  printf("hello, world!\n");
+  assertEq(printf("hello, world!\n"), 14, "hello, world");
   printf("foo%%bar\n", 0x41);
   printf("c:%c:\n", 0x40);
   printf("foo-%sbaz\n", "bar-");
@@ -10,6 +10,7 @@ printf_test() {
   printf("asc('(') = $%x asc(CR) = $%x\n", '(', '\n');
   printf("12345 = %d ; ten = %d ; two = %d ; zero = %d\n",
       12345, 10, 2, 0);
+  printf("0x8000 = %u = %d\n", 0x8000, 0x8000);
   tst_clrchn();
 
   evaluateAsserts();
