41b31012b1104e147f3991fef6c610cf43013434
Philip Zembrod <pzembrod@gmail.com>
2021-12-22 20:52:33 +0100

    Remove duplicate declaration of isdigit() in ctype.h

--- src/lib/include/ctype.h
+++ src/lib/include/ctype.h
@@ -1,6 +1,5 @@
 
 extern _fastcall int isdigit(int c);
-extern _fastcall int isdigit(int c);
 extern _fastcall int isalnum(int c);
 extern _fastcall int isalpha(int c);
 extern _fastcall int isspace(int c);
