3b0585117e83c291124196fda53a208d2a865eba
Philip Zembrod <pzembrod@gmail.com>
2023-12-04 22:53:31 +0100

    Update the remaining Forth kernels for C64 and C16+ to 3.9.5

--- forth/update-forth.sh
+++ forth/update-forth.sh
@@ -4,6 +4,6 @@ set -e
 cc64forthdir="$(realpath --relative-to="$PWD" "$(dirname "${BASH_SOURCE[0]}")")"
 volksforthcbmdir="$(realpath --relative-to="$PWD" "${cc64forthdir}/../../VolksForth/6502/C64/cbmfiles")"
 
-cp "${volksforthcbmdir}/v4th-c64" "${cc64forthdir}/"
-cp "${volksforthcbmdir}/v4th-c16+" "${cc64forthdir}/"
-cp "${volksforthcbmdir}/v4th-x16" "${cc64forthdir}/"
+cp -p "${volksforthcbmdir}/v4th-c64" "${cc64forthdir}/"
+cp -p "${volksforthcbmdir}/v4th-c16+" "${cc64forthdir}/"
+cp -p "${volksforthcbmdir}/v4th-x16" "${cc64forthdir}/"
