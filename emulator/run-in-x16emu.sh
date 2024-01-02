2a86045ed838d621c1a9716af72b71cdda8ee212
Philip Zembrod <pzembrod@gmail.com>
2024-01-02 08:57:01 +0100

    Switch run-in-x16emu.sh to use -fsroot instead of -sdcard

--- emulator/run-in-x16emu.sh
+++ emulator/run-in-x16emu.sh
@@ -7,23 +7,16 @@ x16rom="${X16ROM}"
 executable="${1}"
 keybuf="${2}"
 
+echo executable = $executable
+echo keybuf = $keybuf
+
 emulatordir="$(realpath --relative-to="$PWD" "$(dirname "${BASH_SOURCE[0]}")")"
 source "${emulatordir}/basedir.shlib"
 test -n "${x16filesdir}" || \
   x16filesdir="$(realpath --relative-to="$PWD" "${basedir}/x16files")"
 test -n "${x16rom}" || \
   x16rom="${emulatordir}/x16-c-rom.bin"
-sdcard="${emulatordir}/sdcard.img"
 x16script="${basedir}/tmp/x16script"
-
-make -C "${emulatordir}" sdcard.img
-mformat -i "${sdcard}" -F
-for asciifile in  $(cd "${x16filesdir}" && ls *)
-do
-  # Convert filename to PETSCII, remove trailing CR.
-  petsciifile="$(echo ${asciifile} | ascii2petscii - |tr -d '\r')"
-  mcopy -i "${sdcard}" "${x16filesdir}/${asciifile}" "::${petsciifile}"
-done
 
 autostart=""
 script=""
@@ -51,7 +44,8 @@ then
   ascii2petscii "${x16script}.ascii" "${x16script}.petscii"
   script="-bas ${x16script}.petscii"
   autostart=""
-  mcopy -i "${sdcard}" "${emulatordir}/notdone" "::NOTDONE"
+  # The following could also just be a cp.
+  ascii2petscii "${emulatordir}/notdone" "${x16filesdir}/notdone"
   warp="-warp"
 else
   scale="-scale 2"
@@ -59,8 +53,6 @@ else
 fi
 
 rom=""
-# temporarily disable -rom so r46 uses its default rom until I have
-# figured out a new/better way to generate and set a C charset rom.
 if [ -s "${x16rom}" ]
 then
   rom="-rom ${x16rom}"
@@ -68,7 +60,7 @@ fi
 
 x16emu \
   -keymap de \
-  -sdcard "${sdcard}" \
+  -fsroot "${x16filesdir}" \
   $rom \
   $autostart \
   $script \
@@ -80,7 +72,7 @@ x16emu \
 
 if [ -n "$script" ]
 then
-  while mtype -i "${sdcard}" "::NOTDONE" > /dev/null
+  while [ -f "${x16filesdir}/notdone" ]
     do sleep 1
   done
   sleep 0.5
@@ -97,7 +89,10 @@ wait %1 || echo "x16emu returned $?"
 for outfile in ${OUTFILES}
 do
   sdfile="$(echo "${outfile}"|ascii2petscii - |tr -d '\r')"
-  mcopy -n -i "${sdcard}" "::${sdfile}" "${x16filesdir}/${outfile}"
+  if [ "${x16filesdir}/${sdfile}" != "${x16filesdir}/${outfile}" ]
+  then
+    mv "${x16filesdir}/${sdfile}" "${x16filesdir}/${outfile}"
+  fi
   # Timestamps on the sdcard from x16emu seem to not be reliable.
   # Touch the outfiles so make's dependency analysis works.
   touch "${x16filesdir}/${outfile}"
