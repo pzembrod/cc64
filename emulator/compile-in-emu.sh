cc6ef82a36259ac26569943bb56e92121058b497
Philip Zembrod <pzembrod@gmail.com>
2026-03-05 22:47:59 +0100

    Remove env var HOSTFILES in compile-in-emu.sh in favour of direct use of var CBMFILES

--- emulator/compile-in-emu.sh
+++ emulator/compile-in-emu.sh
@@ -10,14 +10,13 @@ test -n "${OUTFILES}" || export OUTFILES="${program} ${program}.log"
 
 emulatordir="$(realpath --relative-to="$PWD" "$(dirname "${BASH_SOURCE[0]}")")"
 source "${emulatordir}/basedir.shlib"
-hostfiles="$(realpath --relative-to="$PWD" "${basedir}/${CC64HOST}files")"
-test -n "${HOSTFILES}" && hostfiles="${HOSTFILES}"
+cbmfiles="$(realpath --relative-to="$PWD" "${basedir}/${CC64HOST}files")"
+test -n "${CBMFILES}" || export CBMFILES="${cbmfiles}"
 
-logfile="${hostfiles}/${program}.log"
+logfile="${CBMFILES}/${program}.log"
 rm -f "${logfile}"
 keybuf="logfile ${program}.log\ncc ${program}.c\nlogclose\ndos s0:notdone"
 
-export CBMFILES="${hostfiles}"
 "${emulatordir}/run-in-${CC64HOST}emu.sh" "${cc64}" "${keybuf}"
 
 petscii2ascii "${logfile}" | \
--- tests/e2e/compile-in-emu.sh
+++ tests/e2e/compile-in-emu.sh
@@ -4,7 +4,7 @@ set -e
 testdir="$(realpath --relative-to="$PWD" "$(dirname "${BASH_SOURCE[0]}")")"
 source "${testdir}/basedir.shlib"
 emulatordir="$(realpath --relative-to="$PWD" "${basedir}/emulator")"
-HOSTFILES="$(realpath --relative-to="$PWD" "${testdir}/${CC64HOST}files")"
-export HOSTFILES
+CBMFILES="$(realpath --relative-to="$PWD" "${testdir}/${CC64HOST}files")"
+export CBMFILES
 
 "${emulatordir}/compile-in-emu.sh" "$@"
